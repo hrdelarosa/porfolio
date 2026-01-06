602c787102190473efad8d5f30d2954bb04bfdb6
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2026-01-06 01:44:51 -0600

    Agregar archivo navItems.ts y refactorizar Header.astro para importar elementos de navegación desde la configuración

---
+++ src/config/navItems.ts
@@ -0,0 +1,12 @@
+import { socials } from './socials'
+
+export const navItems = [
+  { title: 'Proyectos', label: 'proyectos', url: '/#proyectos' },
+  { title: 'Tecnologías', label: 'tecnologia', url: '/#tecnologia' },
+  { title: 'Sobre Mí', label: 'sobre-mi', url: '/#sobre-mi' },
+  {
+    title: 'Contacto',
+    label: 'contacto',
+    url: socials.email,
+  },
+]
---
+++ src/config/socials.ts
@@ -0,0 +1,4 @@
+export const socials = {
+  linkedin: 'https://www.linkedin.com/in/hozmeldelarosa/',
+  email: 'mailto:hozdelarosar@gmail.com',
+}
