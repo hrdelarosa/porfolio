528c16a0c8a5fa22193bcb421d18a188e0a5be0c
Hozmel Roberto De la Rosa Rendón <hozdelarosar@gmail.com>
2026-01-15 00:21:05 -0600

    Agregar componentes de íconos y configuración de habilidades para mejorar la presentación visual y la funcionalidad del portafolio.

---
+++ src/config/skills.ts
@@ -0,0 +1,43 @@
+import React from '../components/icons/React.astro'
+import TypeScript from '../components/icons/TypeScript.astro'
+import JavaScript from '../components/icons/JavaScript.astro'
+import NextJS from '../components/icons/NextJS.astro'
+import AstroIcon from '../components/icons/AstroIcon.astro'
+import Tailwind from '../components/icons/Tailwind.astro'
+import Git from '../components/icons/Git.astro'
+import GitHub from '../components/icons/GitHub.astro'
+
+export const skills = [
+  {
+    name: 'React',
+    icon: React,
+  },
+  {
+    name: 'TypeScript',
+    icon: TypeScript,
+  },
+  {
+    name: 'JavaScript',
+    icon: JavaScript,
+  },
+  {
+    name: 'Next.js',
+    icon: NextJS,
+  },
+  {
+    name: 'Astro',
+    icon: AstroIcon,
+  },
+  {
+    name: 'Tailwind CSS',
+    icon: Tailwind,
+  },
+  {
+    name: 'Git',
+    icon: Git,
+  },
+  {
+    name: 'GitHub',
+    icon: GitHub,
+  },
+]
